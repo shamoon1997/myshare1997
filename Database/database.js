7c30bda7c34e6c601d6da3f4f7fe9c71000adcf3
shamoon1997 <sshamoon83@gmail.com>
2021-12-30 13:56:43 +0500

    fix untracked

--- Database/database.js
+++ Database/database.js
@@ -4,7 +4,7 @@ const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
-    console.log("Database Connected successfully ");
+    console.log("Database Connected successfully");
 });
 
 module.exports = () => {
